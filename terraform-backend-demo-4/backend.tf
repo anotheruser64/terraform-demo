d682a085da38ce4f7b5f2469dafa6b149cd63c13
anotheruser64 <anotheruser640@gmail>
2026-05-14 18:26:35 +0530

    adding backend file

--- terraform-backend-demo-4/backend.tf
+++ terraform-backend-demo-4/backend.tf
@@ -4,5 +4,6 @@ terraform {
     key = "terraform.tfstate"
     encrypt = true
     region = "us-east-1"
-    dynamodb_table = "terraform_state_lock"}
+    dynamodb_table = "terraform_state_lock"
+    }
 }
