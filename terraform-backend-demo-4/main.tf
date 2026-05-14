d06897296a588ac341a40bf6dd6149a1f1958cd8
anotheruser64 <anotheruser640@gmail>
2026-05-14 18:43:17 +0530

    adding backend file

--- terraform-backend-demo-4/main.tf
+++ terraform-backend-demo-4/main.tf
@@ -2,6 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  
+}
+
 resource "aws_s3_bucket" "aws_s3_creation" {
-  bucket = "aws-anotheruser-my-terraform-state-bucket-2026"
+  bucket = "aws-anotheruser-my-terraform-state-bucket"
 }
