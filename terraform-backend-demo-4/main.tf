9a2123095acdbb63f00e761b1eb58fbdb87c4dbe
anotheruser64 <anotheruser640@gmail>
2026-05-14 18:03:21 +0530

    Backend configuration for tfstate file

---
+++ terraform-backend-demo-4/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  
+}
+
+resource "aws_s3_bucket" "aws_s3_creation" {
+  bucket = "my-terraform-state-bucket"
+}
