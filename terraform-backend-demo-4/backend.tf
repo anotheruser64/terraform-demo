cae46f56df3af995dfbfa83b11b4f90a0e905d52
anotheruser64 <anotheruser640@gmail>
2026-05-14 18:25:23 +0530

    adding backend file

---
+++ terraform-backend-demo-4/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "aws-anotheruser-my-terraform-state-bucket"
+    key = "terraform.tfstate"
+    encrypt = true
+    region = "us-east-1"
+    dynamodb_table = "terraform_state_lock"}
+}
