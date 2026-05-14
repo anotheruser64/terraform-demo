6313c4dbd5e8c830723166fe6d5bad2a20bd73c5
anotheruser64 <anotheruser640@gmail>
2026-05-14 18:16:31 +0530

    removing backend file

--- terraform-backend-demo-4/backend.tf
+++
@@ -1,9 +0,0 @@
-/*terraform {
-  backend "s3" {
-    bucket = "my-terraform-state-bucket"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-    dynamodb_table = "terraform_state_lock"
-  }
-}/*
